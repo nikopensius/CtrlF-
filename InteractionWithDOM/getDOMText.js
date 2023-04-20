30bec9a2478f259951d7d73d5be75943767a44d5
Gustav <gustavnikopensius@gmail.com>
2023-04-20 14:38:49 +0300

    rename extractTextFromDOM.js

--- InteractionWithDOM/getDOMText.js
+++ InteractionWithDOM/getDOMText.js
@@ -9,7 +9,7 @@
  */
 
 
-function extractTextFromDOM() {
+function getDOMText() {
     const elementsToExtract = ["p", "h1", "h2", "h3", "h4", "h5", "h6"]; // add other element types here if needed
     const extractedText = [];
   
