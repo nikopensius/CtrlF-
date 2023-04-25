a1c97b9a5fc8e35967df933c1db605affed9cce7
Gustav <gustavnikopensius@gmail.com>
2023-04-25 22:25:33 +0300

    changes

---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+const path = require('path');
+
+module.exports = {
+  mode: 'development',
+  entry: {
+    background: './src/background.js',
+    content: './src/content.js',
+  },
+  output: {
+    filename: '[name].js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  resolve: {
+    modules: [
+      path.resolve(__dirname, 'node_modules'),
+    ],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+          },
+        },
+      },
+    ],
+  },
+};
