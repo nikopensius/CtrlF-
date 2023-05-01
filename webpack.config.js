20eac9a226725d8fe7e1a5af763d7969adec1dda
Gustav <gustavnikopensius@gmail.com>
2023-05-01 12:28:28 +0300

    messinaround

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path');
 
 module.exports = {
-  mode: 'production',
+  mode: 'development',
   entry: {
     background: './src/background.js',
     content: './src/content.js'
