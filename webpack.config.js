9399ae580d36eafdf6178ee343750ddeb48663c1
Gustav <gustavnikopensius@gmail.com>
2023-05-01 12:10:33 +0300

    webpack mode -> production

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path');
 
 module.exports = {
-  mode: 'development',
+  mode: 'production',
   entry: {
     background: './src/background.js',
     content: './src/content.js'
