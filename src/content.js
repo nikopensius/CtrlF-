79d339fbbcd0706f381e828b82b13cbc0f4a8f03
Gustav <gustavnikopensius@gmail.com>
2023-05-02 16:36:00 +0300

    Reverted to pre-refactor, find bar takes in string

--- src/content.js
+++ src/content.js
@@ -47,20 +47,39 @@ chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
   }
 });
 
+// Define the function that handles the click event outside of the injectFindBar listener
+function handleFindButtonClick(findInput) {
+  const searchString = findInput.value.trim();
+  console.log('User entered search string:', searchString);
+  // TODO: Perform search using the search string
+}
+
 // Listen for messages from the background script
 chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
   if (message.action === 'injectFindBar') {
+    // Check if the find bar already exists
+    if (document.getElementById('tfidf-findbar')) {
+      return sendResponse();
+    }
     // Inject the find bar into the DOM
     const findbar = document.createElement('div');
     findbar.id = 'tfidf-findbar';
     findbar.innerHTML = `
       <input type="text" id="tfidf-findbar-input" placeholder="Search for keywords...">
-      <button id="tfidf-findbar-search">Search</button>
+      <button id="tfidf-findbar-search">Find</button>
     `;
     document.body.appendChild(findbar);
+
+    // Get the find button and text input elements
+    const findButton = document.getElementById('tfidf-findbar-search');
+    const findInput = document.getElementById('tfidf-findbar-input');
+
+    // Add event listener to the find button
+    findButton.addEventListener('click', function() {
+      handleFindButtonClick(findInput);
+    });
 
     // Send a response back to the background script
     sendResponse();
   }
 });
-
