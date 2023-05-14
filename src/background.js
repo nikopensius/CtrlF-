17620e90f2073cecf012479736946aeec2f3aedf
Gustav <gustavnikopensius@gmail.com>
2023-05-14 11:48:50 +0300

    Refactored background listeners

--- src/background.js
+++ src/background.js
@@ -1,10 +1,3 @@
-// import
-import * as Fin from "finnlp";
-
-// instantiate new input
-let processed = new Fin.Run("This is a some text");
-console.log(processed);
-
 // function to find paragraphs that contain all keywords
 function intersection (keywords, invertedIndex) {
   var result = new Set (); // or a hash map
@@ -64,39 +57,19 @@ chrome.commands.onCommand.addListener(command => {
 chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
   if (message.action === 'invertedIndex') {
     // Store the inverted index
-    invertedIndex = message.payload;
-    console.log('Received inverted index:', invertedIndex);
-  } else if (message.action === 'updateKeywordScores') {
-    // Update the keyword scores with the new search query
-    const query = message.payload;
-    keywordScores = {};
-    for (const keyword of query.split(' ')) {
-      if (!invertedIndex.hasOwnProperty(keyword)) {
-        continue;
-      }
-      const documents = Object.keys(invertedIndex[keyword]);
-      const idf = Math.log(Object.keys(invertedIndex).length / documents.length);
-      for (const document of documents) {
-        if (!keywordScores.hasOwnProperty(document)) {
-          keywordScores[document] = 0;
-        }
-        const tf = invertedIndex[keyword][document];
-        keywordScores[document] += tf * idf;
-      }
-    }
-    console.log('Updated keyword scores:', keywordScores);
-    sendResponse();
-  } else if (message.action === 'getHighestScoringParagraphs') {
-    // Get the top N paragraphs with the highest TF-IDF scores
-    const numParagraphs = message.payload.numParagraphs;
-    const paragraphIds = Object.keys(keywordScores).sort((a, b) => keywordScores[b] - keywordScores[a]).slice(0, numParagraphs);
-    sendResponse(paragraphIds);
+    invertedIndexPromise = Promise.resolve(message.payload); // resolve the promise with the inverted index
   } else if (message.action === 'userQuery') {
-    // find paragraphs that contain user query keywords
-    const searchArray = message.payload
-    console.log("Received query words:", searchArray)
-    var paragraph_ids = intersection(searchArray, invertedIndex);
-    sendResponse(paragraph_ids);
+    if (invertedIndexPromise) {
+      invertedIndexPromise.then((invertedIndex) => {
+        console.log("Inverted index:", invertedIndex)
+        // find paragraphs that contain user query keywords
+        const searchArray = message.payload
+        console.log("Received query words:", searchArray)
+        var paragraph_ids = intersection(searchArray, invertedIndex);
+        sendResponse(paragraph_ids);
+      })
+    } else {sendResponse({ error: 'Inverted index not available' }); // Send an error response
+  }
   }
   return true;
 });
